7bfe68c8450b4deab209e79efb9071156ea82c7b
tcutajar <148732209+tcutajar@users.noreply.github.com>
2023-11-18 21:40:20 -0500

    Update sudoku_generator.py

--- sudoku_generator.py
+++ sudoku_generator.py
@@ -137,9 +137,9 @@ class SudokuGenerator:
     '''
     def fill_diagonal(self):
         # Fills the three boxes along the main diagonal of the board
-        fill_box(self, 0,0)
-        fill_box(self, 3,3)
-        fill_box(self, 6,6)
+        fill_box(self, 0, 0)
+        fill_box(self, 3, 3)
+        fill_box(self, 6, 6)
 
     '''
     DO NOT CHANGE
